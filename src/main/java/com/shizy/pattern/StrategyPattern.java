ead7285faff2790d6aacc205d6b1cee1e6884f00
shizhongyu <shizhongyu@yibaogao.com>
2020-08-19 17:13:27 +0800

    add strategy pattern

---
+++ src/main/java/com/shizy/pattern/StrategyPattern.java
@@ -0,0 +1,82 @@
+package com.shizy.pattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 策略模式
+ * <p>
+ * Strategy接口
+ * Strategy类A、B、C
+ * StrategyContext
+ */
+public class StrategyPattern {
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+
+        XxStrategy strategy = new XxStrategyAaa();
+        XxStrategy strategy2 = (XxStrategy) Class.forName("com.shizy.pattern." + "XxStrategyBbb").newInstance();
+
+        XxStrategyContext context = new XxStrategyContext(strategy2);
+        context.execStrategy();
+    }
+
+}
+
+/**
+ * xx策略接口
+ */
+interface XxStrategy {
+    public void doSomething();
+}
+
+/**
+ * xx策略类aaa
+ */
+class XxStrategyAaa implements XxStrategy {
+
+    @Override
+    public void doSomething() {
+        System.out.println("aaa");
+    }
+}
+
+/**
+ * xx策略类bbb
+ */
+class XxStrategyBbb implements XxStrategy {
+
+    @Override
+    public void doSomething() {
+        System.out.println("bbb");
+    }
+}
+
+/**
+ * xx策略类Context
+ */
+@Data
+@AllArgsConstructor
+class XxStrategyContext {
+    private XxStrategy xxStrategy;
+
+    public void execStrategy() {
+        xxStrategy.doSomething();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
