b811e70d09f45ec8085f006ee93ab306f61cfad8
shizy19 <shizy19@meicloud.com>
2020-10-28 17:11:16 +0800

    update lambda demo

--- src/main/java/com/shizy/demo/lambda/LambdaDemo.java
+++ src/main/java/com/shizy/demo/lambda/LambdaDemo.java
@@ -1,31 +1,43 @@
 package com.shizy.demo.lambda;
 
-import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LambdaDemo {
 
-    public static void main(String[] args) {
-//
-        List<?> data = Arrays.asList("a", "b", "c", "d", "e");
+    @Test
+    public void lambdaDemo1() {
 
-        List list1 = data.stream().map(e -> {
-            return e + "" + e;
-        }).collect(Collectors.toList());
+        /**
+         * findFirst
+         * 用一个条件，去list中获取匹配项，得到其po
+         */
 
+        List<Archer> list = new ArrayList<>();
+        list.add(new Archer("ashe", "ice archer", 67));
+        list.add(new Archer("ezreal", "exlporer", 68));
+        list.add(new Archer("vayne", "night hunter", 66));
 
-
-        System.out.println(list1);
+        String findKey = "vayne";
+        Archer archer = list.stream().filter(po -> findKey.equals(po.getName()))
+                .findFirst().orElse(null);
+        String archerTitle = list.stream().filter(po -> findKey.equals(po.getName()))
+                .findFirst().map(Archer::getTitle).orElse(null);
     }
-
 
 }
 
 
-
-
-
-
-
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Archer {
+    private String name;
+    private String title;
+    private int attack;
+}
