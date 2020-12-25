a933016c2ab061f545481e1de77b8459fdfa8382
shizy19 <shizy19@meicloud.com>
2020-12-25 09:13:46 +0800

    [feature] urlTestUtil 获取spring项目中的接口信息，及代码上的注释信息

--- src/main/java/com/shizy/utils/properties/FileContentUtils.java
+++ src/main/java/com/shizy/utils/properties/FileContentUtils.java
@@ -1,6 +1,10 @@
 package com.shizy.utils.properties;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class FileContentUtils {
 
@@ -13,7 +17,7 @@ public class FileContentUtils {
         BufferedReader reader = null;
         StringBuilder sbf = new StringBuilder();
         try {
-            reader = new BufferedReader(new FileReader(file));
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
             String tempStr;
             while ((tempStr = reader.readLine()) != null) {
                 sbf.append(tempStr).append("\r\n");
@@ -49,6 +53,36 @@ public class FileContentUtils {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<File> searchFile(String filePath, String keyWord) {
+        Objects.requireNonNull(filePath);
+        Objects.requireNonNull(keyWord);
+
+        File folder = new File(filePath);// 默认目录
+        if (!folder.exists()) {// 如果文件夹不存在
+            throw new RuntimeException("目录不存在：" + folder.getAbsolutePath());
+        }
+        return searchFile0(folder, keyWord);
+    }
+
+    private static List<File> searchFile0(File folder, final String keyWord) {// 递归查找包含关键字的文件
+        File[] subFolders = folder.listFiles(pathname -> {// 实现FileFilter类的accept方法
+            // 目录或文件包含关键字
+            return pathname.isDirectory()
+                    || (pathname.isFile() && pathname.getName().toLowerCase().equals(keyWord.toLowerCase()));
+        });
+
+        List<File> result = new ArrayList<>();
+        for (File subFolder : Objects.requireNonNull(subFolders)) {// 循环文件夹或文件
+            if (subFolder.isFile()) {// 如果是文件则将文件添加到结果列表中
+                result.add(subFolder);
+            } else {// 如果是文件夹，则递归调用本方法，然后把所有的文件加到结果列表中
+                result.addAll(searchFile0(subFolder, keyWord));
+            }
+        }
+
+        return result;
     }
 
 }
